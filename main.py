7ee49ef3364a250b3d3594ad7d106527256f39a5
wpwebsitecc <wpwebsitecc@gmail.com>
2024-01-24 15:57:19 +0800

    Start sqlalchemy

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
+
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+cursor.execute("INSERT INTO books VALUES(2, 'The Alchemist', 'Paulo Coelho', '9.9')")
+db.commit()
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(new_book)
+
+        # NOTE: You can use the redirect method from flask to redirect to another route
+        # e.g. in this case to the home page after the form has been submitted.
+        return redirect(url_for('home'))
+
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
