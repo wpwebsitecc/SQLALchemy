94ef10ecdbcb315f74b6afb20df6fba71dcb20a4
wpwebsitecc <wpwebsitecc@gmail.com>
2024-01-31 15:36:48 +0800

    Add Editing

---
+++ server.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
+
+app = Flask(__name__)
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+# initialise the app with the extension
+db.init_app(app)
+
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250))
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}'
+
+@app.route('/')
+def home():
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars()
+    return render_template("index.html", books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+
+        # NOTE: You can use the redirect method from flask to redirect to another route
+        # e.g. in this case to the home page after the form has been submitted.
+        return redirect(url_for('home'))
+
+    return render_template("add.html")
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        book_id = request.form['id']
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("editing.html", book=book_selected)
+
+    
+@app.route("/delete", methods=["GET", "POST"])
+def delete():
+    if request.method == "POST":
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
